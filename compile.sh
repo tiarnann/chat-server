20c5838abd57aa751e3bc73f7b2f53b7a3e6c7a3
Tiarnan McGrath <mcgratti@tcd.ie>
2017-10-14 18:07:10 +0100

    Added check for port number arg. Also added support default param

--- compile.sh
+++ compile.sh
@@ -1,2 +1,12 @@
 FLAG="$@"
+
+if [[ $FLAG == "" ]]; then
+    echo "A port number is required when running server. Rerun with \n./compile.sh port_number \n./compile.sh default"
+    exit 1
+elif [[ $FLAG == "default" ]]; then
+    echo "Running server with default port"
+    npm run build && npm start
+    exit 0
+fi
+
 npm run build && port=$FLAG npm start
