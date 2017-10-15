c9bcc86021241d4f361f07e87ab0fe85eaa117f1
Tiarnan McGrath <mcgratti@tcd.ie>
2017-10-15 13:15:55 +0100

    Start of refactor

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,29 @@
 import * as net from "net"
 import * as colour from "chalk"
 import {CSRequestParser, CSRequestType} from "./services/request-parser"
+import App from './app'
 
 // Globals
-const parser = new CSRequestParser()
-
-const server = net.createServer((connection) => {
-    const info = connection.address()
-
-    connection.on('connect',()=>{
-        console.log(colour.green(`Connection established with ${info.address}:${info.port}`));
-    })
-
-    connection.on('data',(data)=>{
-        console.log(colour.yellow(`Recevied data from ${info.address}:${info.port}`))
-        const parsed = parser.parse(data)
-        if(parsed.type == CSRequestType.Echo){
-            connection.write(`${parsed.data.message}`)
-        }
-    })
-
-    connection.on('end',()=>{
-        console.log(colour.red(`Ended connection with ${info.address}:${info.port}`));
-    })
-})
-
+const app = new App()
 const port = parseInt(process.env.port) || 8080
 
-server.listen(port, ()=>{
-    console.log(colour.green(`====\nServer started. Listening on localhost:${port}\n====\n`));s
+// Main requests
+app.on('join',(connection, data)=>{})
+app.on('leave',(connection, data)=>{})
+app.on('message',(connection, data)=>{})
+
+// Extra features
+app.on('echo',(connection, data)=>{
+    console.log(colour.green(`Recevied echo event`))
+})
+
+app.on('kill',(connection, data)=>{})
+
+app.listen(port, ()=>{
+    console.log(colour.green(`====\nServer started. Listening on localhost:${port}\n====\n`));
+
+    let sock = new net.Socket()
+    sock.connect(port, 'localhost', (con)=>{
+        sock.write('HELO test\n')
+    })
 })
