52e78bc997f265cd6dd19e60ec56be5bb6def716
Tiarnan McGrath <mcgratti@tcd.ie>
2017-10-18 00:13:16 +0100

    Multiple args with next into go

--- src/services/Middleware.ts
+++ src/services/Middleware.ts
@@ -1,20 +1,19 @@
-class Middleware{
+export default class Middleware{
     constructor(){}
 
     go(...args) {
-        args.pop()()
+        args[args.length - 1]()
     }
     
     use(fn) {
         const go = this.go 
         
         this.go = function(...args){
-            const fn_next = args.pop() || function(){}
-            args.push(fn_next)
-            const next = fn.bind(null, args)
-            args.push(next)
-            go.bind(this, args).call()
+            const fn_next = args[args.length - 1] || function(){}
+            const next = fn.bind(null, ...args, fn_next)
+            
+            args.pop()
+            go.bind(null, ...args, next).call()
         }
     }
 }
-
