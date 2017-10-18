c4d1f06755c9e5df19e0f1b231f52f5d587d8fe4
Tiarnan McGrath <mcgratti@tcd.ie>
2017-10-18 15:09:38 +0100

    Middleware working with n args, made tests

--- src/services/Middleware.ts
+++ src/services/Middleware.ts
@@ -1,19 +1,31 @@
-export default class Middleware{
+export default class Middleware {
     constructor(){}
 
     go(...args) {
-        args[args.length - 1]()
+        (args[args.length - 1] || function(){})()
     }
     
     use(fn) {
         const go = this.go 
+        const _this = this
         
-        this.go = function(...args){
-            const fn_next = args[args.length - 1] || function(){}
+        this.go = (...args)=> {
+            const lastArg = args[args.length - 1]
+
+            // If there is no middleware left
+            // pass fn a nop
+            let fn_next = function(){}
+
+            if(lastArg instanceof Function){
+                fn_next = lastArg
+
+                // Pop the last function from args so go can be passed fn 
+                args.pop()
+            }
+
             const next = fn.bind(null, ...args, fn_next)
-            
-            args.pop()
-            go.bind(null, ...args, next).call()
+
+            go.call(null, ...args, next)
         }
     }
 }
