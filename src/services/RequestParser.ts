1bff09e66437bc1ae42cc0f359e474215ff26763
Tiarnan McGrath <mcgratti@tcd.ie>
2017-11-21 18:10:23 +0000

    Changed request parsing to be more generic, changed the test too

--- src/services/RequestParser.ts
+++ src/services/RequestParser.ts
@@ -1,54 +1,89 @@
-export interface CSRequest {
-    type: CSRequestType;
-    data;
-}
+import * as ChatRequest from '../models/request'
 
-export enum CSRequestType {
-    Join = 'join',
-    Leave = 'leave',
-    Message = 'message',
-    Echo = 'echo',
-    Kill = 'kill'
-}
+export class ChatRequestParser {
+    private echoRegex = /HELO\s(.+)\n\n/
+    private killRegex = /KILL_SERVICE\n\n/
+    private keyMap = {
+        'JOIN_CHATROOM':'chatroomName',
+        'LEAVE_CHATROOM':'roomRef',
+        'DISCONNECT':'ip',
+        'CHAT':'message',
+        'PORT':'port',
+        'JOIN_ID':'joinId',
+        'CLIENT_NAME':'clientName',
+        'CLIENT_IP':'ip'
+    }
 
-export class CSRequestRegexPattern {
-    static Join = /JOIN_CHATROOM:\s([A-Za-z]*)\nCLIENT_IP:\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|0)\nPORT:\s([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|0)\nCLIENT_NAME:\s([A-Za-z])*\n/g
-    static Echo = /HELO\s(.+)\n/
-    static Kill = /KILL_SERVICE\n/
-}
-
-
-export class CSRequestParser {
     constructor(){}
-    
-    parseEcho(data: string): CSRequest {
-        const parsed = CSRequestRegexPattern.Echo.exec(data)
 
-        if (parsed != null){
-            const data =  {'message': parsed[1]}
-            return {'type': CSRequestType.Echo, 'data': data}
-        }
-        return null
-    }
-
-    parseKill(data: string): CSRequest {
-        const parsed = CSRequestRegexPattern.Kill.exec(data)
-
-        if (parsed != null){
-            return {'type': CSRequestType.Kill, 'data': {}}
-        }
-        return null
-    }
-
-    parse(buffer: Buffer): CSRequest {
+    parse(buffer: Buffer): ChatRequest.ChatRequest {
         const data = buffer.toString()
 
-        if(CSRequestRegexPattern.Echo.test(data)){
-            return this.parseEcho(data)
-        } else if(CSRequestRegexPattern.Kill.test(data)){
-            return this.parseKill(data)
+        // Checking for echo or kill
+        const killRequest = (this.killRegex).exec(data)
+        if(killRequest){
+            return new ChatRequest.ChatRequest(ChatRequest.ChatRequestType.Kill, null, null, null)
+        }
+
+        const echoRequest = (this.echoRegex).exec(data)
+        if(echoRequest){
+            const reply = echoRequest[1] || ""
+            return new ChatRequest.ChatRequest(ChatRequest.ChatRequestType.Echo, {"message": reply}, null, null)
+        }
+
+        // Parse as generic
+        return this.genericParse(data)
+    }
+
+    genericParse(data: string): ChatRequest.ChatRequest{
+        // Transforming "Key: Value\nKey: Value\n\n" into {key: value, key: value}
+        const restructuredData = data.split('\n').reduce((request, line)=> {
+            if(line.length == 0){
+                return request
+            }
+
+            const {'0': key, '1': value} = line.split(': ')
+            request[key] = value
+
+            return request
+        }, {} as ChatRequest.ChatRequestData)
+
+        const type = this.getTypeFrom(restructuredData)
+        
+        if(type){
+            const request = new ChatRequest.ChatRequest(type, restructuredData, null, null)
+            return this.transformDataKeys(request)
         }
 
         return null
+    }
+
+    getTypeFrom(data: ChatRequest.ChatRequestData): ChatRequest.ChatRequestType {
+        if(data['JOIN_CHATROOM']){
+            return ChatRequest.ChatRequestType.Join
+        } else if(data['LEAVE_CHATROOM']){
+            return ChatRequest.ChatRequestType.Leave
+        } else if(data['DISCONNECT']){
+            return ChatRequest.ChatRequestType.Disconnect
+        } else if(data['CHAT']){
+            return ChatRequest.ChatRequestType.Message
+        }
+
+        return null
+    }
+
+    transformDataKeys(request: ChatRequest.ChatRequest): ChatRequest.ChatRequest{
+        const transformedData = Object.keys(request.data).reduce((transformed, key)=>{
+            const transformedKey = this.keyMap[key]
+            const value = request.data[key]
+
+            transformed[transformedKey] = value
+
+            return transformed
+        },{})
+
+        request.data = transformedData
+        
+        return request
     }
 }
--- src/tests/RequestParser-test.ts
+++ src/tests/RequestParser-test.ts
@@ -1,9 +1,9 @@
 import * as assert from 'assert'
-import {CSRequestParser, CSRequestType} from '../services/RequestParser'
-
-const parser = new CSRequestParser()
+import * as Services from '../services/RequestParser'
+import * as Models from '../models/request' 
 
 describe('Request',()=>{
+    const parser = new Services.ChatRequestParser()
 
     it('should return null for a given request with no text', ()=>{
         const message = new Buffer(`HELO\n`)
@@ -14,19 +14,64 @@ describe('Request',()=>{
 
     it('should return correct object for a given request with valid text', ()=>{
         const expectedString = "test"
-        const message = new Buffer(`HELO ${expectedString}\n`)
+        const message = new Buffer(`HELO ${expectedString}\n\n`)
         const obj = parser.parse(message)
 
         assert(obj != null)
-        assert(obj.type === CSRequestType.Echo)
+        assert(obj.type === Models.ChatRequestType.Echo)
         assert(obj.data.message === expectedString)
     })
 
     it('should return a kill object for a given request with KILL_SERVICE text', ()=>{
-        const message = new Buffer(`KILL_SERVICE\n`)
+        const message = new Buffer(`KILL_SERVICE\n\n`)
         const obj = parser.parse(message)
 
         assert(obj != null)
-        assert(obj.type === CSRequestType.Kill)
+        assert(obj.type === Models.ChatRequestType.Kill)
+    })
+
+    it('transform keys using the internal keymap',()=>{
+        let data = {
+            'JOIN_CHATROOM' : '1',
+            'LEAVE_CHATROOM' : '2',
+            'DISCONNECT':'3',
+            'CLIENT_IP':'3',
+            'CHAT':'4',
+            'PORT':'5',
+            'JOIN_ID':'6',
+            'CLIENT_NAME':'7',
+        }
+
+        let request = new Models.ChatRequest(Models.ChatRequestType.Echo, data, '', 0)
+        request = parser.transformDataKeys(request)
+
+        console.log(request)
+        assert(data['JOIN_CHATROOM'] === request.data['chatroomName'])
+        assert(data['LEAVE_CHATROOM'] === request.data['roomRef'])
+        assert(data['DISCONNECT'] === request.data['ip'])
+        assert(data['CHAT'] === request.data['message'])
+        assert(data['PORT'] === request.data['port'])
+        assert(data['JOIN_ID'] === request.data['joinId'])
+        assert(data['CLIENT_IP'] === request.data['ip'])
+    })
+
+    it('transform request into usable objects', ()=>{
+        const rawJoin = `JOIN_CHATROOM: 0\nCLIENT_IP: 1\nPORT: 123\nCLIENT_NAME: 123\n\n`
+        console.log(parser.parse(new Buffer(rawJoin)))
+        
+        let rawLeave = `LEAVE_CHATROOM: 0\nJOIN_ID: 123\nCLIENT_NAME: username\n\n`
+        console.log(parser.parse(new Buffer(rawLeave)))
+
+        let rawDisconnect = `DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: username\n\n`
+        console.log(parser.parse(new Buffer(rawDisconnect)))
+
+        let rawChat = `CHAT: 0\nJOIN_ID: 0\nCLIENT_NAME: 0\nMESSAGE: bkjsabd askdnasjkdb asdkjnaskjdb\n\n`
+        console.log(parser.parse(new Buffer(rawChat)))
+        
+        let rawKill = `KILL_SERVICE\n\n`
+        console.log(parser.parse(new Buffer(rawKill)))
+
+        let rawHello = `HELO text\n\n`
+        console.log(parser.parse(new Buffer(rawHello)))
     })
 })
