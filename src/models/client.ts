1d6bb5bef9319c829e032e549b51040d32d2062f
Tiarnan McGrath <mcgratti@tcd.ie>
2017-11-24 00:12:31 +0000

    Client now has connection member

--- src/models/client.ts
+++ src/models/client.ts
@@ -1,3 +1,5 @@
+import * as net from 'net'
+
 export class Client {
-    constructor(public name: string, public address: string, public port: number, public joinId: number, public connection?){}
+    constructor(public name: string, public address: string, public port: number, public joinId: number, public connection?: net.Socket){}
 }
