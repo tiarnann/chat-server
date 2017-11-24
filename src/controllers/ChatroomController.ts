29edf1880aec745dccb262c866bcb4528be7fc15
Tiarnan McGrath <mcgratti@tcd.ie>
2017-11-24 00:41:08 +0000

    Now adding some event handling logic to test

--- src/controllers/ChatroomController.ts
+++ src/controllers/ChatroomController.ts
@@ -15,56 +15,106 @@ export default class ChatroomController {
         this.clientsController = new ClientsController() 
     }
 
-    handleJoin(request: Requests.ChatRequest): Messages.JoinedChatroomMessage {
+    handleJoin(request: Requests.ChatRequest): boolean {
+        // Extracting request content
+        const socket = request.connection
         const joinRequest = request.data as Requests.JoinChatroomRequest
         const {clientName, chatroomName} = joinRequest
+
+        // Retrieving Client
         const joinId = this.clientsController.getClientJoinId(clientName)
-        
         let client = this.clientsController.getClient(joinId)
 
+        // Creating client if it does not exist
         if(joinId == null){
-            client = this.clientsController.addClient(clientName)
+            client = this.clientsController.addClient(clientName, socket)
         }
 
+        // Creating chatroom
         const chat = this.chatsController.addChat(chatroomName)
         
         if(chat == null){
             return null
         }
         
+        // Adding client to Chatroom
         chat.clients[joinId] = client
-        
-        return new Messages.JoinedChatroomMessage(chatroomName, chat.reference,client.joinId,'0',0)
+
+        // Replying to client
+        const joinMessage = new Messages.JoinedChatroomMessage(chatroomName, chat.reference, client.joinId, '0', 0)
+        socket.write(joinMessage.toString)
     }
 
     handleLeave(request: Requests.ChatRequest): boolean {
         const leaveRequest = request.data as Requests.LeaveChatroomRequest
         const {clientName, joinId, roomRef} = leaveRequest
         
-        const clientExists = this.clientsController.clientExistsWithName(clientName)
+        // Retrieving Client
+        let client = this.clientsController.getClient(joinId)
 
-        if(!clientExists){
+        if(client == null){
             return false
         }
 
         const chat = this.chatsController.getChat(roomRef)
 
-        if(!!chat){
+        if(chat == null){
             return false
         }
 
+        // Remove client
         delete chat.clients[joinId]
-        
+
+        // Replying to client
+        const leaveMessage = new Messages.LeftChatroomMessage(roomRef, joinId).toString()
+
+        // Broadcast event to all clients within chat
+        Object.keys(chat.clients).forEach(key => {
+            const client = chat.clients[key]
+            client.connection.write(leaveMessage)
+        })
+
         return true
     }
 
-    handleDisconnect(request: Requests.ChatRequest){
+    handleDisconnect(request: Requests.ChatRequest): boolean{
         const disconnectRequest = request.data as Requests.DisconnectChatroomRequest
         const {clientName} = disconnectRequest
+
+        // Retrieving Client
+        const joinId = this.clientsController.getClientJoinId(clientName)
+        let client = this.clientsController.getClient(joinId)
+
+        // Creating client if it does not exist
+        if(joinId == null){
+            // send back error
+        }
+
+        // Disconnect user
+        request.connection.end()
+
+        const chats = this.chatsController.chats
+
+
+        Object.keys(chats).forEach(chatKey => {
+            const chat = chats[chatKey]
+
+            const leaveMessage = new Messages.LeftChatroomMessage(chat['roomRef'], joinId).toString()
+            
+            // Broadcast event to all clients within chat
+            Object.keys(chat.clients).forEach(key => {
+                const client = chat.clients[key]
+                client.connection.write(leaveMessage)
+            })
+        })
+        
+
+        return true
     }
     
-    handleMessage(request: Requests.ChatRequest){
+    handleMessage(request: Requests.ChatRequest): boolean{
         const messageRequest = request.data as Requests.MessageChatroomRequest
         const {clientName, joinId, message, roomRef} = messageRequest
+        return true
     }
 }
